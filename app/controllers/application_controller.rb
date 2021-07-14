21fe276eb00e4c7c8e8b1bbd77d9f651fb34f034
bambi teague <teeegsa@gmail.com>
2021-07-13 22:39:09 -0500

    Reuse of sessions/omniauth code from another project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  #this will give us access to these methods in our views
+  helper_method :current_user, :logged_in?, :redirect_if_not_logged_in
+
+  private
+
+  def current_user
+    @current_user ||= Globaluser.find(session[:globaluser_id]) if session[:globaluser_id]
+  end
+
+  def logged_in?
+    !!session[:globaluser_id]
+  end
+
+  def redirect_if_not_logged_in
+    redirect_to '/' if !logged_in?
+  end
+
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,33 @@
+class SessionsController < ApplicationController
+  
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+
+  def create
+    user = Globaluser.find_by(username: params[:globaluser][:username])
+    if user && user.authenticate(params[:globaluser][:password])
+      session[:globaluser_id] = user.id
+      redirect_to profile_path
+    else
+      flash[:message] = "Incorrect login information" 
+      redirect_to '/login'
+    end
+  end
+
+  def omniauth
+    #processing the response we recieved from Google and either:
+    #create a new user 
+    #or 
+    #log in an existing user
+    user = Globaluser.from_omniauth(request.env['omniauth.auth'])
+    if user.valid?
+      session[:globaluser_id] = user.id
+      redirect_to complete_path
+    else
+      redirect_to '/login'
+    end
+  end
+
+end
