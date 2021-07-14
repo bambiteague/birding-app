8be3b5395b8c3eec3418228c258e16d6d0c6593a
bambi teague <teeegsa@gmail.com>
2021-07-13 22:42:18 -0500

    generating controllers

---
+++ app/helpers/bird_uploads_helper.rb
@@ -0,0 +1,2 @@
+module BirdUploadsHelper
+end
---
+++ test/controllers/bird_uploads_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class BirdUploadsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
