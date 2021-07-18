d39aca3924c46148b76c47803f347254bbd58b82
bambi teague <teeegsa@gmail.com>
2021-07-18 18:38:14 -0500

    made errors display

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -2,7 +2,7 @@ class Bird < ApplicationRecord
   has_many :sightings
   has_many :users, through: :sightings
 
-  validates :species, presence: true, uniqueness: {message: "is already listed. Please select a species from our drop down of previously recorded birds"}
-  validates :quantity, presence: true, numericality: {greater_than: 0, less_than: 101}, {message: "limit of birds for a sighting is 100"}
+  validates :species, presence: {message: "is required"}, uniqueness: {message: "is already listed in our collection"}
+  validates :quantity, presence: {message: "is required"}, numericality: {greater_than: 0, less_than: 101, message: "limit of birds for a sighting is 100"}
 
 end
--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -4,6 +4,6 @@ class Sighting < ApplicationRecord
   
   accepts_nested_attributes_for :bird
 
-  validates :location, presence: true, {message: "required to record a sighting"}
-  validates :date_spotted, presence: true, {message: "required to record a sighting"}
+  validates :location, presence: {message: "required to record a sighting"}
+  validates :date_spotted, presence: {message: "required to record a sighting"}
 end
