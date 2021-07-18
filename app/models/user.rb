ceebd413265020b83ce174c03a8e7cb1b949dbc1
bambi teague <teeegsa@gmail.com>
2021-07-18 18:29:41 -0500

    cleaned up validations in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,10 @@ class User < ApplicationRecord
 
   #something about my validations below messed up login / commented out for now ----->
   
-      validates :username, presence: true, {message: "is required"}
-      validates :username, uniqueness: true, {message: "already in use"}
-      validates :location, presence: true, {message: "please let us know where you're birding from"}
-      validates :email, presence: true, uniqueness: true, {message: "already in use"}
-      validates :password, presence: true, {message: "is required"}
-      validates :password,  length: { in: 6..20 }, {message: "keep between 6-20 characters"}
+      validates :username, presence: {message: "is required"}, uniqueness: {message: "already in use"}
+      validates :location, presence: {message: "please let us know where you're birding from"}
+      validates :email, presence: {message: "is required"}, uniqueness: {message: "already in use"}
+      validates :password, presence: {message: "is required"}, length: {in: 6..20, message: "keep between 6-20 characters"}
 
   def self.from_omniauth(response)
     User.find_or_create_by(uid: response[:uid], provider: response[:provider]) do |u|
